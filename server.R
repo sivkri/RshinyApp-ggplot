27bfcb16a897179cade54b4de2a8e0bc97c27ef6
Sivakumar Krishnamoorthy <indiangene@gmail.com>
2023-04-12 12:15:57 +0200

    Add files via upload

---
+++ server.R
@@ -0,0 +1,21 @@
+# load shiny library
+library(shiny)
+
+#runExample("07_widgets")
+setwd("N:/HuR/testApp")
+
+# set up back end
+shinyServer(function(input, output) {
+  # load the data
+  amlData <- read.delim("shinyExampleData.tsv")
+  
+  # construct a plot to show the data
+  library(ggplot2)
+  output$scatterPlot <- renderPlot({
+    p1 <- ggplot(amlData, aes_string(x=input$x_axis, y=input$y_axis, colour=input$point_color)) + geom_point()
+    p1 <- p1 + xlab("Variant Allele Fraction") + ylab("Variant Allele Fraction")
+    p1
+  })
+  
+  output$text <- renderText({"This is some text"})
+})
