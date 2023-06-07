25dd9ff3adf347c296da24fc1c6db6abf3ec9997
Sivakumar Krishnamoorthy <indiangene@gmail.com>
2023-06-07 16:09:22 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,21 +1,26 @@
-# load shiny library
+# Load the shiny library
 library(shiny)
 
-#runExample("07_widgets")
+# Set the working directory
 setwd("N:/HuR/testApp")
 
-# set up back end
+# Set up the server
 shinyServer(function(input, output) {
-  # load the data
+  
+  # Load the data
   amlData <- read.delim("shinyExampleData.tsv")
   
-  # construct a plot to show the data
-  library(ggplot2)
+  # Construct a plot to show the data
   output$scatterPlot <- renderPlot({
-    p1 <- ggplot(amlData, aes_string(x=input$x_axis, y=input$y_axis, colour=input$point_color)) + geom_point()
+    library(ggplot2)
+    p1 <- ggplot(amlData, aes_string(x = input$x_axis, y = input$y_axis, colour = input$point_color)) +
+      geom_point()
     p1 <- p1 + xlab("Variant Allele Fraction") + ylab("Variant Allele Fraction")
     p1
   })
   
-  output$text <- renderText({"This is some text"})
+  # Render some text
+  output$text <- renderText({
+    "This is some text"
+  })
 })
